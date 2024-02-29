f62d07ad13c6f66d3322c8149389f6064c2c5db7
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-02-29 17:43:20 +0100

    backend project file

---
+++ backend/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings  # Corrected import
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('userauth.urls')),
+    path('api/exams/', include('exams.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
