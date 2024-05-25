31fdac0c3677ade69f1936aa45f32818e77e5241
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-25 19:02:51 +0100

    modified serializers for exams

--- exams/urls.py
+++ exams/urls.py
@@ -1,18 +1,35 @@
+# from django.urls import path
+# from .views import (
+#     CreateQuestionAPIView,
+#     RetrieveQuestionAPIView,
+#     UpdateQuestionAPIView,
+#     DeleteQuestionAPIView,
+#     FilteredQuestionsAPIView,
+#     # ListAllQuestionsAPIView
+# )
+
+# urlpatterns = [
+#     path('questions/create/', CreateQuestionAPIView.as_view(), name='create-question'),
+#     path('questions/<int:pk>/', RetrieveQuestionAPIView.as_view(), name='retrieve-question'),
+#     path('questions/<int:pk>/update/', UpdateQuestionAPIView.as_view(), name='update-question'),
+#     path('questions/<int:pk>/delete/', DeleteQuestionAPIView.as_view(), name='delete-question'),
+#     path('questions/filter/', FilteredQuestionsAPIView.as_view(), name='filtered-questions'),
+#     # path('questions/all/', ListAllQuestionsAPIView.as_view(), name='list-all-questions'),
+# ]
+
+
 from django.urls import path
 from .views import (
-    CreateQuestionAPIView,
-    RetrieveQuestionAPIView,
-    UpdateQuestionAPIView,
-    DeleteQuestionAPIView,
-    FilteredQuestionsAPIView,
-    # ListAllQuestionsAPIView
-)
+    CreateWAECQuestionAPIView, 
+    RetrieveWAECQuestionAPIView, 
+    UpdateWAECQuestionAPIView, 
+    DeleteWAECQuestionAPIView,
+    ListWAECQuestionAPIView)
 
 urlpatterns = [
-    path('questions/create/', CreateQuestionAPIView.as_view(), name='create-question'),
-    path('questions/<int:pk>/', RetrieveQuestionAPIView.as_view(), name='retrieve-question'),
-    path('questions/<int:pk>/update/', UpdateQuestionAPIView.as_view(), name='update-question'),
-    path('questions/<int:pk>/delete/', DeleteQuestionAPIView.as_view(), name='delete-question'),
-    path('questions/filter/', FilteredQuestionsAPIView.as_view(), name='filtered-questions'),
-    # path('questions/all/', ListAllQuestionsAPIView.as_view(), name='list-all-questions'),
+     path('waec-questions/', ListWAECQuestionAPIView.as_view(), name='list-questions'),
+    path('waec-questions/create/', CreateWAECQuestionAPIView.as_view(), name='create-question'),
+    path('waec-questions/<int:pk>/', RetrieveWAECQuestionAPIView.as_view(), name='retrieve-question'),
+    path('waec-questions/<int:pk>/update/', UpdateWAECQuestionAPIView.as_view(), name='update-question'),
+    path('waec-questions/<int:pk>/delete/', DeleteWAECQuestionAPIView.as_view(), name='delete-question'),
 ]
