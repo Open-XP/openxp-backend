8c066cdcd644f7396720877a629597b665d7ad8e
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-25 18:45:47 +0100

    added quiz manager urls to urls path

--- backend/urls.py
+++ backend/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('userauth.urls')),
     path('api/exams/', include('exams.urls')),
+    path('api/quiz/', include('quiz_manager.urls')),
 ]
 
 if settings.DEBUG:
