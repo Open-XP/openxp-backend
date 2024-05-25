45ca7ddd68428519735fbc76f3987e591c8d98df
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-25 18:47:27 +0100

    added filters to the exam questions by year and subjects

--- exams/admin.py
+++ exams/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Subject, Exam, Topic, Question, Grade
+# from .models import Subject, Exam, Topic, Question, Grade
+from .waec_model import WAEC, Questions, Subject, Year
 
+# admin.site.register(Subject)
+# admin.site.register(Exam)
+# admin.site.register(Topic)
+# admin.site.register(Question)
+# admin.site.register(Grade)
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display = ('question', 'subject', 'year', 'answer')
+    list_filter = ('subject', 'year')
+
+admin.site.register(WAEC)
+admin.site.register(Year)
 admin.site.register(Subject)
-admin.site.register(Exam)
-admin.site.register(Topic)
-admin.site.register(Question)
-admin.site.register(Grade)
+admin.site.register(Questions, QuestionsAdmin)
+
