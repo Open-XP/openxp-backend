a332a69633dc3882607f8bd2232125651ce9e0dd
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-26 11:35:39 +0100

    modified serailizer for questionserializer

--- quiz_manager/serializers.py
+++ quiz_manager/serializers.py
@@ -1,23 +1,28 @@
 from rest_framework import serializers
 from .models import TestInstance, UserAnswer, Questions, UserScore
 
+
 class TestInstanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestInstance
         fields = ['id', 'user', 'exam', 'subject', 'year', 'start_time', 'end_time', 'is_completed']
         read_only_fields = ['start_time', 'end_time', 'is_completed']
 
+
 class UserAnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAnswer
         fields = '__all__'
 
+
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Questions
-        fields =  ['id', 'image', 'question', 'option_A', 'option_B', 'option_C', 'option_D', 'option_E', 'subject', 'year']
+        fields = '__all__'
+        # exclude = ['answer',]
+        # fields =  ['id', 'image', 'question', 'option_A', 'option_B', 'option_C', 'option_D', 'option_E',  'subject', 'year']
+        
     
-
 
 class UserScoreSerializer(serializers.ModelSerializer):
     correct_questions = QuestionSerializer(many=True, read_only=True)
