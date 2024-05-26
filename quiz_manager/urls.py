77f9e33e3461f6c35da988b373f9364686f5967f
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-26 02:10:33 +0100

    Created API endpoints

---
+++ quiz_manager/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from .views import (
+    StartTestAPIView,
+    RetrieveQuestionsAPIView,
+    SubmitAnswerAPIView,
+    CompleteTestAPIView,
+    TestResultAPIView,
+    CompletedTestsAPIView,
+    RetrieveAllTestInstancesAPIView,
+    DeleteTestInstanceAPIView,
+    RetrieveIndividualQuestionsAPIView,
+    UserScoreAPIView,
+)
+
+urlpatterns = [
+    path('exams/start-test/', StartTestAPIView.as_view(), name='start-test'),
+    path('exams/all-test-instances/', RetrieveAllTestInstancesAPIView.as_view(), name='all-test-instances'),
+    path('exams/test-instances/<int:test_instance_id>/questions/<int:question_id>/', RetrieveIndividualQuestionsAPIView.as_view(), name='retrieve-individual-question'),
+    path('exams/<int:pk>/delete/', DeleteTestInstanceAPIView.as_view(), name='delete-test-instance'),
+    path('exams/<int:test_instance_id>/questions/', RetrieveQuestionsAPIView.as_view(), name='retrieve-questions'),
+    path('exams/submit-answer/', SubmitAnswerAPIView.as_view(), name='submit-answer'),
+    path('exams/complete-test/<int:pk>/', CompleteTestAPIView.as_view(), name='complete-test'),
+    path('exams/<int:test_instance_id>/results/', TestResultAPIView.as_view(), name='test-results'),
+    path('user-score/<int:test_instance_id>/', UserScoreAPIView.as_view(), name='user-score'),
+    path('exams/completed-tests/', CompletedTestsAPIView.as_view(), name='completed-tests'),
+]
