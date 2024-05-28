299d3316b1de794e2719a21d5c00d887df16a35d
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-28 01:27:57 +0100

    added new models for time tracking

--- quiz_manager/models.py
+++ quiz_manager/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.conf import settings
 from exams.waec_model import WAEC, Questions, Subject, Year
 from datetime import timezone
+from datetime import timedelta
 
 class TestInstance(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='test_instances')
     exam = models.ForeignKey(WAEC, on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
-    year = models.ForeignKey(Year, on_delete=models.PROTECT)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    year = models.ForeignKey(Year, on_delete=models.CASCADE)
     questions = models.ManyToManyField(Questions)
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(null=True, blank=True)
@@ -40,12 +41,20 @@ class UserAnswer(models.Model):
         return f"Answer to {self.question.question} by {self.test_instance.user}: {correct_status}"
 
     
-    
 class UserScore(models.Model):
     score = models.IntegerField(null=True, blank=True)
+    total_time = models.CharField(max_length=50, null=True, blank=True)
     test_instance = models.ForeignKey(TestInstance, on_delete=models.CASCADE, related_name='user_scores')
     correct_questions = models.ManyToManyField(Questions, related_name='correct_in_scores', blank=True)
     incorrect_questions = models.ManyToManyField(Questions, related_name='incorrect_in_scores', blank=True)
 
     def __str__(self):
         return f"Score for {self.test_instance}: {self.score}"
+
+class TotalStudyTime(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    overall_study_time = models.DurationField(default=timedelta(seconds=0))  # Correctly using timedelta here
+    last_updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Total study time for {self.user.username}: {self.overall_study_time}"
