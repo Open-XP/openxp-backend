ed2c233cba2d4e3660837c6e3e6d88771bba7fb0
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-28 01:24:29 +0100

    added time tracking to admin panel && git push

--- quiz_manager/admin.py
+++ quiz_manager/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import UserAnswer, TestInstance, UserScore
+from .models import UserAnswer, TestInstance, UserScore, TotalStudyTime
 
 
 admin.site.register(UserAnswer)
 admin.site.register(TestInstance)
 admin.site.register(UserScore)
+admin.site.register(TotalStudyTime)
 
