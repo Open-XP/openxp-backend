2ec34ca83b96b85583a7ec026373eda1239c0c93
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-28 01:23:37 +0100

    added time tracking for test duration and for total study time

--- exams/views.py
+++ exams/views.py
@@ -1,106 +1,46 @@
-# from rest_framework.response import Response
-# from rest_framework import status
-# from .models import Question
-# from .serializers import QuestionSerializer
-# from rest_framework import generics
-# from serializers import WAECQuestionSerializer
-# from rest_framework.permissions import IsAuthenticated, AllowAny
-# from .permissions import IsUploader
-# from rest_framework.views import APIView
-# from rest_framework.pagination import PageNumberPagination
-    
-    
-# class CreateQuestionAPIView(generics.CreateAPIView):
-#     queryset = Questions.objects.all()
-#     serializer_class = QuestionSerializer
-#     permission_classes = [IsUploader]
-
-#     def perform_create(self, serializer):
-#         serializer.save(uploader=self.request.user)
-
-
-# class RetrieveQuestionAPIView(generics.RetrieveAPIView):
-#     queryset = Questions.objects.all()
-#     serializer_class = QuestionSerializer
-#     permission_classes = [IsUploader]
-
-
-# class UpdateQuestionAPIView(generics.UpdateAPIView):
-#     queryset = Questions.objects.all()
-#     serializer_class = QuestionSerializer
-#     permission_classes = [IsUploader]
-
-
-# class DeleteQuestionAPIView(generics.DestroyAPIView):
-#     queryset = Questions.objects.all()
-#     serializer_class = QuestionSerializer
-#     permission_classes = [IsUploader]
-
-
-# class StandardResultsPagination(PageNumberPagination):
-#     page_size = 10
-#     page_size_query_param = 'page_size'
-#     max_page_size = 100
-
-
-# class FilteredQuestionsAPIView(APIView):
-#     def get(self, request):
-#         queryset = Questions.objects.all()
-
-#         # Retrieve query parameters
-#         year = request.query_params.get('year')
-#         exam_type_name = request.query_params.get('exam_type')
-#         subject_name = request.query_params.get('subject')
-#         topic_name = request.query_params.get('topic')
-
-#         # Apply filters if they exist
-#         if year:
-#             queryset = queryset.filter(uploaded_at__year=year)
-#         if exam_type_name:
-#             queryset = queryset.filter(exam_type__name__icontains=exam_type_name)
-#         if subject_name:
-#             queryset = queryset.filter(subject__name__icontains=subject_name)
-#         if topic_name:
-#             queryset = queryset.filter(topic__name__icontains=topic_name)
-
-#         # Pagination
-#         paginator = StandardResultsPagination()
-#         paginated_queryset = paginator.paginate_queryset(queryset, request)
-#         serializer = QuestionSerializer(paginated_queryset, many=True)
-#         return paginator.get_paginated_response(serializer.data)
-
-
 from rest_framework import generics
 from .waec_model import Questions 
 from .serializers import WAECQuestionSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .permissions import IsUploader
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import QuestionsFilter
+from rest_framework.pagination import PageNumberPagination
 
 class CreateWAECQuestionAPIView(generics.CreateAPIView):
     queryset = Questions.objects.all()
     serializer_class = WAECQuestionSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsUploader]
     
+
+class StandardResultsPagination(PageNumberPagination):
+    page_size = 10  # Number of items per page
+    page_size_query_param = 'page_size'  # Allow client to specify the page size
+    max_page_size = 20 
+
 
 class ListWAECQuestionAPIView(generics.ListAPIView):
     queryset = Questions.objects.all()
     serializer_class = WAECQuestionSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsUploader]
     filter_backends = [DjangoFilterBackend]
     filterset_class = QuestionsFilter
+    pagination_class = StandardResultsPagination
+
 
 class RetrieveWAECQuestionAPIView(generics.RetrieveAPIView):
     queryset = Questions.objects.all()
     serializer_class = WAECQuestionSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsUploader]
+
 
 class UpdateWAECQuestionAPIView(generics.UpdateAPIView):
     queryset = Questions.objects.all()
     serializer_class = WAECQuestionSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsUploader]
+
 
 class DeleteWAECQuestionAPIView(generics.DestroyAPIView):
     queryset = Questions.objects.all()
     serializer_class = WAECQuestionSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsUploader]
