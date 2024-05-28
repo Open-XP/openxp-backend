0fb7e4a5b1882a3acd3043bfa5adf4a53a23ac2c
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-28 01:22:42 +0100

    added MSQLdb

--- backend/settings.py
+++ backend/settings.py
@@ -121,6 +121,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'openxpdb', 
+        # 'USER' : 'root',
+        # 'PASSWORD': 'maniZolas1008',
+        # 'PORT': '3306',
+        # 'HOST': '127.0.0.1',
     }
 }
 
