2763a0e1f4f0555e1e7ed239c84fca4e01384bcc
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-28 01:33:38 +0100

    modified authentication endpoints

--- userauth/urls.py
+++ userauth/urls.py
@@ -12,15 +12,13 @@ from .views import (RegistrationAPIView,
                     UserLogoutAPIView)
 
 urlpatterns = [
-    # authentication endpoints
-    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('user/register', RegistrationAPIView.as_view(), name='register'),
-    path('user/verify-email', VerifyEmailAPIView.as_view(), name='verify_email'),
-    path('user/resend-verify-email', ResendVerifyEmailAPIView.as_view(), name='resend_verify_email'),
-    path('user/login', UserLoginAPIView.as_view(), name='login'),
-    path('user/logout', UserLogoutAPIView.as_view()),
-
+    path('user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/register/', RegistrationAPIView.as_view(), name='register'),
+    path('user/verify-email/', VerifyEmailAPIView.as_view(), name='verify_email'),
+    path('user/resend-verify-email/', ResendVerifyEmailAPIView.as_view(), name='resend_verify_email'),
+    path('user/login/', UserLoginAPIView.as_view(), name='login'),
+    path('user/logout/', UserLogoutAPIView.as_view()),
     path('user/me', UserAPIView.as_view(), name='user'),
 
 ]
