94e412c6cdca982691dfb7bb7b7dc1ce18021f64
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-16 15:11:51 +0100

    modified django to serve react app

--- backend/urls.py
+++ backend/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings  # Corrected import
+from django.conf import settings 
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('userauth.urls')),
     path('api/exams/', include('exams.urls')),
     path('api/quiz/', include('quiz_manager.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
