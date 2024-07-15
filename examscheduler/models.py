9c69f78a5444793bd3c6cdb4a2664fc8e337f059
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-07-15 22:14:14 +0100

    feat: Add ExamScheduler model for managing exams

---
+++ examscheduler/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.conf import settings
+
+from django.db import models
+
+class ExamScheduler(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    exam_type = models.CharField(max_length=100) 
+
+    def __str__(self):
+        return f"{self.user} - {self.subject} - {self.date} - {self.exam_type}"
