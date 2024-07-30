c0794bd27c5f2a4dbd28232423c1e08a0ece0686
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-07-30 20:02:53 +0100

    feat: Update allowed hosts and add AI module for handling AI API requests and responses

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +10,8 @@ urlpatterns = [
     path('api/exams/', include('exams.urls')),
     path('api/quiz/', include('quiz_manager.urls')),
     path('api/examscheduler/', include('examscheduler.urls')),
+    path('api/ai/', include('ai.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
     # path('', include('main.urls')),
 ]
 
