0f33c4d81d5ae38544e520dd9188900bac47888b
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-08-27 02:36:46 +0100

    chore: Add delete functionality for chat sessions

--- ai/urls.py
+++ ai/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import ExplainAnswersView, ChatSessionListCreateView, ChatSessionDetailView, ChatView, CareerSuggestionsView
+from .views import (
+    ExplainAnswersView,
+    ChatSessionListCreateView,
+    ChatSessionDetailView,
+    ChatView,
+    CareerSuggestionsView,
+    ChatSessionDeleteView
+    )
 
 urlpatterns = [
     path('explain_answers/', ExplainAnswersView.as_view(), name='explain_answers'),
     path('chat/<int:session_id>/', ChatView.as_view(), name='chat'),
     path('sessions/', ChatSessionListCreateView.as_view(), name='list_chat_sessions'),
     path('sessions/<int:id>/', ChatSessionDetailView.as_view(), name='chat_session_detail'),
+    path('sessions/delete/<int:id>/', ChatSessionDeleteView.as_view(), name='delete_chat_session'),
      path('career-suggestions/', CareerSuggestionsView.as_view(), name='career-suggestions'),
 ]
