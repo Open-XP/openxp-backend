02847fe526251e4dd3389c3c5df14f57431f9a02
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-09-03 17:32:05 +0100

    feat: Add new views for generating specific content, detailed notes, and creating test instances

--- ai/urls.py
+++ ai/urls.py
@@ -6,7 +6,12 @@ from .views import (
     ChatView,
     CareerSuggestionsView,
     ChatSessionDeleteView,
-    GenerateQuestionsView
+    GenerateSpecificContentView,
+    GenerateLearningContentContainerView,
+    GenerateDetailedNoteView,
+    GenerateQuestionsAndCreateTestView,
+    ListQuestionsView,
+    TestInstanceQuestionsView, 
     )
 
 urlpatterns = [
@@ -15,6 +20,11 @@ urlpatterns = [
     path('sessions/', ChatSessionListCreateView.as_view(), name='list_chat_sessions'),
     path('sessions/<int:id>/', ChatSessionDetailView.as_view(), name='chat_session_detail'),
     path('sessions/delete/<int:id>/', ChatSessionDeleteView.as_view(), name='delete_chat_session'),
-     path('career-suggestions/', CareerSuggestionsView.as_view(), name='career-suggestions'),
-     path('generate-questions/', GenerateQuestionsView.as_view(), name='generate_questions'),
+    path('career-suggestions/', CareerSuggestionsView.as_view(), name='career-suggestions'),
+    path('generate-learning-content/', GenerateLearningContentContainerView.as_view(), name='generate_learning_content_container'),
+    path('generate-specific-content/', GenerateSpecificContentView.as_view(), name='generate_specific_content'),
+    path('generate-detailed-note/', GenerateDetailedNoteView.as_view(), name='generate_detailed_note'),
+    path('generate-questions-create-test/', GenerateQuestionsAndCreateTestView.as_view(), name='generate_questions_create_test'),
+    path('generated-questions/', ListQuestionsView.as_view(), name='list_questions'),
+    path('generated-test-instance/<str:test_instance_id>/', TestInstanceQuestionsView.as_view(), name='test-instance-questions'),
 ]
