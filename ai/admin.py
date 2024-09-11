07d712d09c9a047e0b76a863d0077f64519a345c
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-09-11 10:40:44 +0100

    Refactor admin.py to register TestInstance model

--- ai/admin.py
+++ ai/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import ChatMessage, ChatSession, Subject, Topic    
+from .models import ChatMessage, ChatSession, Subject, Topic, TestInstance    
 
 admin.site.register(ChatSession)
 admin.site.register(ChatMessage)
 admin.site.register(Subject)
 admin.site.register(Topic)
+admin.site.register(TestInstance)
+
